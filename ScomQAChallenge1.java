68f2d7cd46989909fe5026977f31ed33be657db0
The-Oracle-77 <bernard.ouma21@gmail.com>
2019-05-10 12:54:23 +0300

    Final work

--- ScomQAChallenge1.java
+++ ScomQAChallenge1.java
@@ -1,9 +1,13 @@
 import java.util.*;
-
+/*
+Simple program to print a product of user input and a count that goes
+upto ten(10)
+ */
 
 public class ScomQAChallenge1 {
 
     public static void main(String args[]) {
+        System.out.println("Input a number and press ENTER KEY :-");
         Scanner myObj = new Scanner(System.in);
         int x  = myObj.nextInt();
         int count = 0;
@@ -11,10 +15,16 @@ public class ScomQAChallenge1 {
 
         while( count < 10 ) {
 
-            count++;
-            System.out.print(x+" x  " + count+" = " + x*count );
+           if (x < 21) {
+               count++;
+               System.out.print(x + " x  " + count + " = " + x * count);
 
-            System.out.print("\n");
+               System.out.print("\n");
+           }
+           else{
+               System.out.print("The number needs to be between 1 and 20");
+               System.exit(0);
+           }
         }
     }
 }
--- ScomQAChallenge2.java
+++ ScomQAChallenge2.java
@@ -3,18 +3,27 @@ import java.util.Scanner;
 
 public class ScomQAChallenge2 {
 
+    /*
+    This program returns the least palindrome number
+    that is greater than the imputed figure by the user
+     */
     public static void main(String[] args) {
+        System.out.println("Input a number and press ENTER KEY :-");
         Scanner myObj = new Scanner(System.in);
         int start  = myObj.nextInt();
         int b;
+        int base = start;
         int counter = 0;
         //int start = 2;
         while (counter < 1) {
 
             b=Integer.toString(start).length();
+
+            //validate that the entry qualifies to be a palindrome by at least being two digit number
+
             if (b > 1) {
                 if (isPrime(start) && isPalin(start)) {
-                    System.out.print(start + " ");
+                    System.out.print("The nearest Prime Number Palindrome  to "+base+" is "+start + ".");
                     counter++;
                     if (counter % 10 == 0) {
                         System.out.println();
@@ -27,7 +36,13 @@ public class ScomQAChallenge2 {
 
     public static boolean isPalin(int x) {
         String s = Integer.toString(x);
+
         for(int i = 0; i < s.length()/2; i++) {
+            /*
+            after converting to string, check that the characters at opposite
+            positions match
+            If they do, return true, else return false
+             */
             if(s.charAt(i) != s.charAt(s.length()-i-1)) {
                 return false;
             }
@@ -35,6 +50,9 @@ public class ScomQAChallenge2 {
         return true;
     }
     public static boolean isPrime(int x) {
+        //prime number validation
+        //apart from 2, all other prime numbers are odd numbers
+        //below, check for prime numbers
         if (x % 2 == 0 && x != 2) {
             return false;
         }
